c4aa3e5208a349e86a59048a970e63d0e4c88185
Bruno Gonzalez <stenyak@stenyak.com>
2004-08-24 19:09:01 +0000

    Added the MSVC 2003 project files.

---
+++ src/prj/msvc2003/copy_debug_files.bat
@@ -0,0 +1,8 @@
+xcopy /y /E ..\..\..\3pl\windows\vc2003\lib\Debug\*.dll .\bin\Debug\
+xcopy /y ..\..\..\3pl\windows\vc2003\lib\xerces-c_2_5_0D.dll .\bin\Debug\
+
+xcopy /y ..\..\*Config.xml .\bin\Debug
+
+mkdir .\bin\data
+xcopy /y /E ..\..\..\data .\bin\data
+
