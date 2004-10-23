097c5acfda389ce54759b2a35f78baf69ca6f8fc
Mattias Hellström <heson@lysator.liu.se>
2004-10-23 15:46:00 +0000

    Modified main program: engines are now local variables in the main scope instead of new/delete heap objects. (by Heson)

--- src/main.cpp
+++ src/main.cpp
@@ -40,86 +40,72 @@
 int main (int argc, char **argv)
 {
     // We start the main log engine.
-    XmlFile * xmlFile = new XmlFile ("mainConfig.xml");
-    LogEngine * log = processXmlRootNode (xmlFile->getRootNode());
-    delete xmlFile;
+    XmlFile xmlFile("mainConfig.xml");
+    LogEngine * log = processXmlRootNode (xmlFile.getRootNode());
 
     // We declare the 'global' data and engines.
     log->put (LOG_INFO, "( 1 ): Loading engines and libraries...");
     log->put (LOG_INFO, "Creating system data");
-    new SystemData ();
+    SystemData systemData;
+
     log->put (LOG_INFO, "Creating graphics engine");
-    GraphicsEngine *graphicsEngine = new GraphicsEngine ();
+    GraphicsEngine graphicsEngine;
     log->put (LOG_INFO, "Creating physics engine");
-    PhysicsEngine *physicsEngine = new PhysicsEngine ();
+    PhysicsEngine physicsEngine;
     log->put (LOG_INFO, "Creating gui engine");
-    GuiEngine *guiEngine = new GuiEngine ();
+    GuiEngine guiEngine;
 
     log->put (LOG_INFO, "Starting SDL subsystems");
     startSdl (log);
     log->put (LOG_INFO, "Creating input engine");
-    InputEngine *inputEngine = new InputEngine ();
-
-    log->put (LOG_INFO, "Getting system data pointer");
-    SystemData *systemData = SystemData::getSystemDataPointer ();
+    InputEngine inputEngine;
 
     // We load the world data from hdd into memory.
     log->put (LOG_INFO, "( 2 ): Loading world data...");
     log->put (LOG_INFO, "Loading initial world data");
-    new World("../data/worlds/default.xml");
+    World world("../data/worlds/default.xml");
 
     // We start the main loop.
     log->put (LOG_INFO, "( 3 ): Starting simulation...");
     log->put (LOG_INFO, "Initializating main loop");
-    systemData->statisticsTime = systemData->simulationTime = SDL_GetTicks ();
+    systemData.statisticsTime = systemData.simulationTime = SDL_GetTicks ();
     log->put (LOG_INFO, "Enabling main loop");
-    systemData->enableMainLoop ();
+    systemData.enableMainLoop ();
     log->put (LOG_INFO, "Starting main loop");
-    while (systemData->isMainLoopEnabled ())
+    while (systemData.isMainLoopEnabled ())
     {
         // Update current real loop time.
-        systemData->realTime = SDL_GetTicks ();
+        systemData.realTime = SDL_GetTicks ();
         // Update statistics data every second.
-        if (systemData->simulationTime - systemData->statisticsTime >= 1000)
+        if (systemData.simulationTime - systemData.statisticsTime >= 1000)
         {
-            systemData->graphicsFrequency = systemData->graphicsSteps;
-            systemData->physicsFrequency = systemData->physicsSteps;
-            systemData->graphicsSteps = systemData->physicsSteps = 0;
-            systemData->statisticsTime += 1000;
-            log->format (LOG_VERBOSE, "Main Loop Stats: graphicsFps=%i - physicsFps=%i", systemData->graphicsFrequency, systemData->physicsFrequency);
+            systemData.graphicsFrequency = systemData.graphicsSteps;
+            systemData.physicsFrequency = systemData.physicsSteps;
+            systemData.graphicsSteps = systemData.physicsSteps = 0;
+            systemData.statisticsTime += 1000;
+            log->format (LOG_VERBOSE, "Main Loop Stats: graphicsFps=%i - physicsFps=%i", systemData.graphicsFrequency, systemData.physicsFrequency);
         }
         // Run the physics engine until the game time is in sync with the real loop time.
         
-        while (((systemData->realTime - systemData->simulationTime) >= systemData->physicsTimeStep) && (systemData->isMainLoopEnabled ()))
+        while (((systemData.realTime - systemData.simulationTime) >= systemData.physicsTimeStep) && (systemData.isMainLoopEnabled ()))
         {
-            systemData->simulationTime += systemData->physicsTimeStep;
-            systemData->physicsSteps++;
-            physicsEngine->computeStep ();
-            inputEngine->computeStep ();
+            systemData.simulationTime += systemData.physicsTimeStep;
+            systemData.physicsSteps++;
+            physicsEngine.computeStep();
+            inputEngine.computeStep();
         }
         // Run the gui engine.
-        guiEngine->computeStep ();
+        guiEngine.computeStep ();
         // Run the graphics engine.
-        graphicsEngine->computeStep ();
-        systemData->graphicsSteps++;
+        graphicsEngine.computeStep ();
+        systemData.graphicsSteps++;
     }
     log->put (LOG_INFO, "Main loop finished");
 
     // We unload the world data from memory.
     log->put (LOG_INFO, "( 4 ): Unloading world data...");
-    log->put (LOG_INFO, "Unloading world data");
-    delete World::getWorldPointer();
-
     // We delete the 'global' data and engines.
     log->put (LOG_INFO, "( 5 ): Unloading engines and libraries...");
-    log->put (LOG_INFO, "Deleting graphics engine");
-    delete graphicsEngine;
-    log->put (LOG_INFO, "Deleting physics engine");
-    delete physicsEngine;
-    log->put (LOG_INFO, "Deleting input engine");
-    delete inputEngine;
-    log->put (LOG_INFO, "Deleting gui engine");
-    delete guiEngine;
     log->put (LOG_INFO, "Deleting log engine");
     delete log;
     stopSdl ();
@@ -128,23 +114,27 @@ int main (int argc, char **argv)
     return (0);
 }
 
+
+
 void startSdl (LogEngine * log)
 {
-    if (SDL_Init (SDL_INIT_EVERYTHING) < 0)
-    {
-        log->format (LOG_ERROR, "Couldn't initialize SDL: %s\n", SDL_GetError ());
-    }
+  if (SDL_Init (SDL_INIT_EVERYTHING) < 0) {
+    log->format (LOG_ERROR, "Couldn't itialize SDL: %s\n", SDL_GetError ());
+  }
 }
+
+
 
 void stopSdl (void)
 {
-    SDL_Quit ();
+  SDL_Quit ();
 }
+
 
 LogEngine * processXmlRootNode (DOMNode * node)
 {
-    LogEngine * log = new LogEngine (LOG_TRACE, "XML");
-    log->put(LOG_INFO, "Assigning default values");
+    LogEngine log (LOG_TRACE, "XML");
+    log.put (LOG_INFO, "Assigning default values");
     LOG_LEVEL localLogLevel = LOG_TRACE;
     std::string localLogName = "MAI";
     
@@ -153,11 +143,11 @@ LogEngine * processXmlRootNode (DOMNode * node)
         if (node->getNodeType () == DOMNode::ELEMENT_NODE)
         {
             char *name = XMLString::transcode (node->getNodeName ());
-            log->format (LOG_INFO, "Name: %s", name);
+            log.format (LOG_INFO, "Name: %s", name);
 
             if (!strncmp (name, "mainConfig", 11))
             {
-                log->put (LOG_INFO, "Found the main config element.");
+                log.put (LOG_INFO, "Found the main config element.");
                 if (node->hasAttributes ())
                 {
                     // get all the attributes of the node
@@ -172,7 +162,7 @@ LogEngine * processXmlRootNode (DOMNode * node)
                         if (name == "localLogLevel")
                         {
                             name.erase();
-                            log->format (LOG_INFO, "\tFound the local log level: %s", name.c_str());
+                            log.format (LOG_INFO, "\tFound the local log level: %s", name.c_str());
                             name = XMLString::transcode (pAttributeNode->getValue ());
                             if (name == "LOG_ERROR")
                                 localLogLevel = LOG_ERROR;
@@ -189,7 +179,7 @@ LogEngine * processXmlRootNode (DOMNode * node)
                         {
                             name.erase();
                             name = XMLString::transcode (pAttributeNode->getValue ());
-                            log->format (LOG_INFO, "\tFound the log name: %s", name.c_str());
+                            log.format (LOG_INFO, "\tFound the log name: %s", name.c_str());
 
                             localLogName.erase();
                             localLogName = name;
@@ -202,7 +192,6 @@ LogEngine * processXmlRootNode (DOMNode * node)
         }
     }
     LogEngine *returnLog = new LogEngine (localLogLevel, localLogName.c_str());
-    delete log;
     returnLog->put (LOG_INFO, "Temporary parsing data already loaded into memory...");
     returnLog->put (LOG_INFO, "Unloading temporary parsing data from memory...");
     localLogName.erase();
