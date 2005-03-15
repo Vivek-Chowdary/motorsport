7770d6c7fc96804e589d01f62dda82ae303b7961
Kirk F. House <mckirkus@yahoo.com>
2005-03-15 01:18:02 +0000

    Patched the MSVC project file, release and debug .bat files as well as graphicsEngine.cpp - Thanks to Edgardo Somaschini aka Homogesalt

--- src/prj/msvc2003/copy_debug_files.bat
+++ src/prj/msvc2003/copy_debug_files.bat
@@ -1,5 +1,4 @@
 xcopy /y /E ..\..\..\3pl\windows\vc2003\lib\Debug\*.dll .\bin\Debug\
-xcopy /y ..\..\..\3pl\windows\vc2003\lib\xerces-c_2_5_0D.dll .\bin\Debug\
 
 xcopy /y ..\..\*Config.xml .\bin\Debug
 xcopy /y ..\..\*Config.xsd .\bin\Debug
