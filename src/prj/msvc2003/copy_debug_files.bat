7823e06735da5d8debe2675afa265791cd1def8a
Kirk F. House <mckirkus@yahoo.com>
2006-04-20 03:46:01 +0000

    Updated vs2003 batch files to work with the new 3pl directory structure.

--- src/prj/msvc2003/copy_debug_files.bat
+++ src/prj/msvc2003/copy_debug_files.bat
@@ -1,15 +1,17 @@
 xcopy /y /E ..\..\..\3pl\windows\vc2003\lib\Debug\*.dll .\bin\Debug\
 
+xcopy /y /E %OGRE_HOME%\bin\debug\*.dll .\bin\debug
+
 xcopy /y ..\..\*Config.xml .\bin\Debug
 xcopy /y ..\..\*Config.xsd .\bin\Debug
 
-REM allow local overrides in the project directory
-REM
 
-xcopy /y .\*Config.xml .\bin\Debug
 
 mkdir .\bin\data
 xcopy /y /E ..\..\..\data .\bin\data
 
 mkdir .\bin\cfg
 xcopy /y /E ..\..\..\cfg .\bin\cfg
+
+REM allow local overrides in the project directory
+REM
--- src/prj/msvc2003/copy_release_files.bat
+++ src/prj/msvc2003/copy_release_files.bat
@@ -1,12 +1,10 @@
 xcopy /y /E ..\..\..\3pl\windows\vc2003\lib\Release\*.dll .\bin\Release\
 
+xcopy /y /E %OGRE_HOME%\bin\release\*.dll .\bin\release
+
 xcopy /y ..\..\*Config.xml .\bin\Release
 xcopy /y ..\..\*Config.xsd .\bin\Release
 
-REM allow local overrides in the project directory
-REM
-
-xcopy /y .\*Config.xml .\bin\Release
 
 mkdir .\bin\data
 xcopy /y /E ..\..\..\data .\bin\data
@@ -14,3 +12,8 @@ xcopy /y /E ..\..\..\data .\bin\data
 
 mkdir .\bin\cfg
 xcopy /y /E ..\..\..\cfg .\bin\cfg
+
+REM allow local overrides in the project directory
+REM
+
+xcopy /y .\*Config.xml .\bin\cfg
